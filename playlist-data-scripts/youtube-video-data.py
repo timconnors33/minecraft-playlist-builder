afe4f3a5e17fa828f7f7db0b7c73c4324d7c6311
timconnors33 <72170902+timconnors33@users.noreply.github.com>
2025-01-23 09:55:24 -0500

    Get playlist data from YouTube API

--- playlist-data-scripts/youtube-video-data.py
+++ playlist-data-scripts/youtube-video-data.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 import json
+import googleapiclient.discovery
 
 
 load_dotenv(find_dotenv())
@@ -17,18 +18,24 @@ class Season():
         self.series_title = series_title
 
 class Video():
-     def __init__(self, channel_id, channel_name, title, season_title, series_title, thumbnail_uri):
+     def __init__(self, channel_id, title, season_title, series_title, thumbnail_uri):
         self.channel_id = channel_id
-        self.channel_name = channel_name
         self.title = title,
         self.season_title = season_title
         self.series_title = series_title
         self.thumbnail_uri = thumbnail_uri
 
 class SeasonAppearance():
-    def __init__(self, channel_id, season_title):
+    def __init__(self, channel_id, season_title, video_count):
         self.channel_id = channel_id
         self.season = season_title
+        self.video_count = video_count
+
+class Channel():
+    def __init__(self, id, name, thumbnail):
+        self.id = id
+        self.name = name
+        self.thumbnail = thumbnail
 
 def getWikiData():
     season_appearances = []
@@ -44,6 +51,20 @@ def processWikiData():
     season_appearances = getWikiData()
     for season_appearance in season_appearances:
         youtube_link = season_appearance['youtube_internal_link']
-        '''if youtube_link == 'playlist?list=PLSCZsQa9VSCc-7-qOc8O7t9ZraR4L5y0Y':'''
+        if youtube_link == 'playlist?list=PLSCZsQa9VSCc-7-qOc8O7t9ZraR4L5y0Y':
+            youtube_link = youtube_link.replace('playlist?list=', '')
+            processPlaylistVideos(youtube_link)
+
+def processPlaylistVideos(playlist_id):
+    youtube = googleapiclient.discovery.build(
+        serviceName='youtube', version='v3', developerKey=YOUTUBE_API_KEY
+    )
+    request = youtube.playlistItems().list(
+        part='snippet',
+        maxResults=50,
+        playlistId=playlist_id
+    )
+    response = request.execute()
+    print(response)
 
 processWikiData()
