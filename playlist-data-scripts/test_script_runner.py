900219bc909291e181603f213e9923397bfa732d
timconnors33 <72170902+timconnors33@users.noreply.github.com>
2025-04-15 16:18:48 -0400

    Implement basic testing of uploading objects to database

--- playlist-data-scripts/test_script_runner.py
+++ playlist-data-scripts/test_script_runner.py
@@ -8,7 +8,7 @@ import db_api
 load_dotenv(find_dotenv())
 db_conn_str = os.environ.get('DEV_ODBC_DB_CONNECTION_STRING')
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def setupTeardownDevDatabase():
     cleanUpDatabase()
     initializeDatabase()
@@ -36,6 +36,45 @@ def cleanUpDatabase():
     
 def test_playlist_pagination(setupTeardownDevDatabase):
     scar_channel = [c for c in db_api.getChannels() if c.ChannelYouTubeId == 'UCodkNmk9oWRTIYZdr_HuSlg'].pop()
-    scar_season_appearance = [s for s in db_api.getSeasonAppearances() if s.ChannelId == scar_channel.ChannelId].pop()
+    hermitcraft_season_9 = [s for s in db_api.getSeasons() if s.SeasonTitle == 'Season 9' and s.SeriesTitle == 'Hermitcraft'].pop()
+    scar_season_appearance = [s for s in db_api.getSeasonAppearances() if s.ChannelId == scar_channel.ChannelId and s.SeasonId == hermitcraft_season_9.SeasonId].pop()
     scar_videos = [v for v in db_api.getVideos() if v.SeasonAppearanceId == scar_season_appearance.SeasonAppearanceId]
     assert len(scar_videos) > 50
+    
+def test_series_upload(setupTeardownDevDatabase):
+    series = db_api.getSeries()
+    matching_series = [s for s in series if s.SeriesTitle == 'Hermitcraft']
+    
+    assert len(series) == 2
+    assert len(matching_series) == 1
+
+def test_season_upload(setupTeardownDevDatabase):
+    seasons = db_api.getSeasons()
+    matching_seasons = [s for s in seasons if s.SeasonTitle == 'Season 9' and s.SeriesTitle == 'Hermitcraft']
+    
+    assert len(seasons) == 2
+    assert len(matching_seasons) == 1
+
+def test_channel_upload(setupTeardownDevDatabase):
+    channels = db_api.getChannels()
+    matching_scar_channels = [c for c in channels if c.ChannelYouTubeId == 'UCodkNmk9oWRTIYZdr_HuSlg']
+    matching_bdubs_channels = [c for c in channels if c.ChannelYouTubeId == 'UClu2e7S8atp6tG2galK9hgg']
+    matching_etho_channels = [c for c in channels if c.ChannelYouTubeId == 'UCFKDEp9si4RmHFWJW1vYsMA']
+    
+    assert len(matching_scar_channels) == 1
+    assert len(matching_bdubs_channels) == 1
+    assert len(matching_etho_channels) == 1
+    assert len(channels) == 3
+
+def test_season_appearance_upload(setupTeardownDevDatabase):
+    season_appearances = db_api.getSeasonAppearances()
+    scar_channel = [c for c in db_api.getChannels() if c.ChannelYouTubeId == 'UCodkNmk9oWRTIYZdr_HuSlg'].pop()
+    scar_season_appearances = [s for s in season_appearances if s.ChannelId == scar_channel.ChannelId]
+    hermitcraft_season_9 = [s for s in db_api.getSeasons() if s.SeasonTitle == 'Season 9' and s.SeriesTitle == 'Hermitcraft'].pop()
+    third_life = [s for s in db_api.getSeasons() if s.SeasonTitle == 'Third Life' and s.SeriesTitle == 'Life Series'].pop()
+    
+    assert len(season_appearances) == 4
+    assert len(scar_season_appearances) == 2
+    assert len([s for s in scar_season_appearances if s.SeasonId == hermitcraft_season_9.SeasonId]) == 1
+    assert len([s for s in scar_season_appearances if s.SeasonId == third_life.SeasonId]) == 1
+    
